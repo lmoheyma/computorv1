8bca0bc8ade9a3528c4e3e3583198be049a372a0
lmoheyma <louismoheymani@gmail.com>
2024-05-30 22:45:52 +0200

    ✅ Sign for coefficient

--- src/solver.c
+++ src/solver.c
@@ -6,13 +6,22 @@
 /*   By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 20:14:01 by lmoheyma          #+#    #+#             */
-/*   Updated: 2024/05/30 21:00:14 by lmoheyma         ###   ########.fr       */
+/*   Updated: 2024/05/30 22:45:00 by lmoheyma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "computorv1.h"
 
-t_coef reduce_side(t_polynomial *terms) {
+static void change_sign(t_polynomial *terms) {
+	t_polynomial *temp = terms;
+
+	while (temp) {
+		(temp->sign == '-' ? temp->coefficient *= -1 : 0);
+		temp = temp->next;
+	}
+}
+
+static t_coef reduce_side(t_polynomial *terms) {
 	t_coef coefs;
 	
 	while (terms) {
@@ -31,6 +40,8 @@ void reduce_equation(t_polynomial *left_terms, t_polynomial *right_terms) {
 	t_coef left_coef;
 	t_coef right_coef;
 	
+	change_sign(left_terms);
+	change_sign(right_terms);
 	left_coef = reduce_side(left_terms);
 	printf("a = %.1f, b = %.1f, c = %.1f\n", left_coef.a, left_coef.b, left_coef.c);
 	right_coef = reduce_side(right_terms);
