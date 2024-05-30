20132c90592e619bce0b849a069597612a508153
lmoheyma <louismoheymani@gmail.com>
2024-05-30 23:56:15 +0200

    ✅ Reduced form diplay

--- src/solver.c
+++ src/solver.c
@@ -6,11 +6,24 @@
 /*   By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 20:14:01 by lmoheyma          #+#    #+#             */
-/*   Updated: 2024/05/30 22:45:00 by lmoheyma         ###   ########.fr       */
+/*   Updated: 2024/05/30 23:54:27 by lmoheyma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "computorv1.h"
+
+static void display_reduced(t_coef left_coefs, t_polynomial *terms) {
+	(void)terms;
+
+	printf("Reduced form: ");
+	(left_coefs.a != 0.0) ? printf("%.1f * X^2", left_coefs.a) : 0;
+	(left_coefs.a != 0.0 && left_coefs.b != 0.0) ? printf(" + ") : 0;
+	(left_coefs.b != 0.0) ? printf("%.1f * X", left_coefs.b) : 0;
+	((left_coefs.b != 0.0 && left_coefs.c != 0.0) 
+		|| (left_coefs.a != 0.0 && left_coefs.c != 0.0)) ? printf(" + ") : 0;
+	(left_coefs.c != 0.0) ? printf("%.1f", left_coefs.c) : 0;
+	printf(" = 0\n");
+}
 
 static void change_sign(t_polynomial *terms) {
 	t_polynomial *temp = terms;
@@ -21,29 +34,33 @@ static void change_sign(t_polynomial *terms) {
 	}
 }
 
-static t_coef reduce_side(t_polynomial *terms) {
-	t_coef coefs;
+static t_coef *reduce_side(t_polynomial *terms, t_coef *coefs) {
+	// t_coef coefs;
 	
 	while (terms) {
 		if (terms->exponent == 0)
-			coefs.c += terms->coefficient;
+			coefs->c += terms->coefficient;
 		else if (terms->exponent == 1)
-			coefs.b += terms->coefficient;
+			coefs->b += terms->coefficient;
 		else
-			coefs.a += terms->coefficient;
+			coefs->a += terms->coefficient;
 		terms = terms->next;
 	}
 	return (coefs);
 }
 
 void reduce_equation(t_polynomial *left_terms, t_polynomial *right_terms) {
-	t_coef left_coef;
-	t_coef right_coef;
+	t_coef left_coef = {0.0, 0.0, 0.0};
+	t_coef right_coef = {0.0, 0.0, 0.0};
 	
 	change_sign(left_terms);
 	change_sign(right_terms);
-	left_coef = reduce_side(left_terms);
+	reduce_side(left_terms, &left_coef);
+	reduce_side(right_terms, &right_coef);
+	(right_coef.a != 0.0) ? left_coef.a -= right_coef.a : 0;
+	(right_coef.b != 0.0) ? left_coef.b -= right_coef.b : 0;
+	(right_coef.c != 0.0) ? left_coef.c -= right_coef.c : 0;
+	right_coef = (t_coef){0.0, 0.0, 0.0};
 	printf("a = %.1f, b = %.1f, c = %.1f\n", left_coef.a, left_coef.b, left_coef.c);
-	right_coef = reduce_side(right_terms);
-	printf("a = %.1f, b = %.1f, c = %.1f\n", right_coef.a, right_coef.b, right_coef.c);
+	display_reduced(left_coef, left_terms);
 }
